1d0a198882e10cd504408b3ff461bbe816681a0f
Hieu Nguyen <hieund898@gmail.com>
2024-06-20 06:25:10 +0000

    feat: add helllo

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 
+console.log('Hello World');
 
 
 
@@ -36,4 +37,6 @@ let app = express();
 
 
 
- module.exports = app;
+
+
+module.exports = app;
